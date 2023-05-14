0fcd9316475be17020ad1ee66b5c367255ddbe5e
seita1996 <taha.seita@gmail.com>
2023-05-14 23:18:37 +0900

    Add button to statusbar

--- src/extension.ts
+++ src/extension.ts
@@ -4,22 +4,27 @@ import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+export function activate({ subscriptions }: vscode.ExtensionContext) {
+	
 	console.log('Congratulations, your extension "vscode-gitlab-task-list" is now active!');
+	
+	const commandId = 'vscode-gitlab-task-list.notify';
+	
+	_showStatusBarItem(commandId);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-gitlab-task-list.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-gitlab-task-list!');
+	// show a message box when the command is called
+	let disposable = vscode.commands.registerCommand(commandId, () => {
+		vscode.window.showInformationMessage('nofify!!');
 	});
+	subscriptions.push(disposable);
+}
 
-	context.subscriptions.push(disposable);
+function _showStatusBarItem(commandId: string) {
+	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	statusBarItem.command = commandId;
+	statusBarItem.tooltip = 'View GitLab Task List';
+	statusBarItem.text = '$(checklist) GitLabTL';
+	statusBarItem.show();
 }
 
 // This method is called when your extension is deactivated
