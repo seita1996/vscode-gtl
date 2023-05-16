62210deb5c5c096540f626955db18e13d611c3db
seita1996 <taha.seita@gmail.com>
2023-05-16 22:46:02 +0900

    Drawing notification cards based on JSON data

--- src/NotificationsViewProvider.ts
+++ src/NotificationsViewProvider.ts
@@ -25,43 +25,6 @@ export class NotificationsViewProvider implements vscode.WebviewViewProvider {
     }
 
     private _getHtmlForWebview(scriptUri: vscode.Uri, styleUri: vscode.Uri) {
-        const jsonData = [
-            {
-                "name": "John Doe",
-                "age": 30,
-                "city": "New York"
-            },
-            {
-                "name": "Jane Smith",
-                "age": 25,
-                "city": "London"
-            },
-            {
-                "name": "Bob Johnson",
-                "age": 40,
-                "city": "Osaka"
-            }
-        ];
-
-        // const jsonData = [
-        //     {
-        //         id: 1,
-        //         title: 'this is title',
-        //         description: 'this is description',
-        //         type: 'MR',
-        //         avatar: 'https://avatars.githubusercontent.com/u/19267892?v=4',
-        //         url: 'https://gitlab.com',
-        //     },
-        //     {
-        //         id: 2,
-        //         title: 'this is title2',
-        //         description: 'this is description2',
-        //         type: 'MR',
-        //         avatar: 'https://avatars.githubusercontent.com/u/19267892?v=4',
-        //         url: 'https://gitlab.com',
-        //     },
-        // ];
-        
         return `
             <!DOCTYPE html>
             <html lang="ja">
@@ -72,37 +35,6 @@ export class NotificationsViewProvider implements vscode.WebviewViewProvider {
                 <link rel="stylesheet" href="${styleUri}">
             </head>
             <body>
-                <div class="card">
-                    <div class="card-header">
-                        <div class="flex-between mb-4">
-                            <div class="notification__repository">
-                                this is repository name
-                            </div>
-                            <div class="notification__badge">
-                                4
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div class="flex-start mb-4">
-                            <div class="notification__icon mr-4">
-                                <img src="https://avatars.githubusercontent.com/u/19267892?v=4" alt="Avatar" />
-                            </div>
-                            <div class="notification__title">
-                                this is title
-                            </div>
-                        </div>
-                        <div class="flex-start">
-                            <div class="notification__tag mr-4">
-                                MR
-                            </div>
-                            <div class="notification__type">
-                                you are assigned
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
                 <div id="output"></div>
                 <script src="${scriptUri}" />
             </body>
