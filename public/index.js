e26bceb8ed03c42dd73e94cc66ab2f1ab9e6f3c6
seita1996 <taha.seita@gmail.com>
2023-05-16 22:27:48 +0900

    Set WebView style

--- public/index.js
+++ public/index.js
@@ -1,2 +1,34 @@
-const app = document.getElementById("app");
-app.innerText = "Hello World!";
+const jsonData = [
+    {
+        "name": "John Doe",
+        "age": 30,
+        "city": "New York"
+    },
+    {
+        "name": "Jane Smith",
+        "age": 25,
+        "city": "London"
+    },
+    {
+        "name": "Bob Johnson",
+        "age": 40,
+        "city": "Osaka"
+    }
+];
+
+function displayJSONData(data) {
+    var outputDiv = document.getElementById("output");
+    var html = "";
+
+    for (var i = 0; i < data.length; i++) {
+        html += "<p>Name: " + data[i].name + "</p>";
+        html += "<p>Age: " + data[i].age + "</p>";
+        html += "<p>City: " + data[i].city + "</p>";
+        html += "<hr>";
+    }
+
+    outputDiv.innerHTML = html;
+}
+
+// JSONデータの表示を実行
+displayJSONData(jsonData);
--- src/NotificationsViewProvider.ts
+++ src/NotificationsViewProvider.ts
@@ -6,8 +6,8 @@ export class NotificationsViewProvider implements vscode.WebviewViewProvider {
     public resolveWebviewView(webviewView: vscode.WebviewView) {
         // Set up the webview view
         webviewView.webview.options = {
-        enableScripts: true,
-        enableCommandUris: true
+            enableScripts: true,
+            enableCommandUris: true
         };
 
         // WebView 内で`./public/index.js`を読み込み可能にするためのUri
@@ -21,38 +21,92 @@ export class NotificationsViewProvider implements vscode.WebviewViewProvider {
         );
 
         // Load the content of the view
-        webviewView.webview.html = `
-        <!DOCTYPE html>
-        <html lang="ja">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>WebView Example</title>
-            <link rel="stylesheet" href="${styleUri}">
-        </head>
-        <body>
-            <div class="card">
-                <div class="card-header">
-                    <h2>this is title</h2>
-                </div>
-                <div class="card-body">
-                    <div class="notification__icon">
-                        <img src="https://avatars.githubusercontent.com/u/19267892?v=4" alt="Avatar" />
-                    </div>
-                    <div class="notification__tag">
-                        MR
-                    </div>
-                    <div class="notification__description">
-                        this is description
-                    </div>
-                </div>
-                <div id="app" />
-            </div>
+        webviewView.webview.html = this._getHtmlForWebview(scriptUri, styleUri);
+    }
 
-            <script src="${scriptUri}" />
-        </body>
-        </html>
+    private _getHtmlForWebview(scriptUri: vscode.Uri, styleUri: vscode.Uri) {
+        const jsonData = [
+            {
+                "name": "John Doe",
+                "age": 30,
+                "city": "New York"
+            },
+            {
+                "name": "Jane Smith",
+                "age": 25,
+                "city": "London"
+            },
+            {
+                "name": "Bob Johnson",
+                "age": 40,
+                "city": "Osaka"
+            }
+        ];
+
+        // const jsonData = [
+        //     {
+        //         id: 1,
+        //         title: 'this is title',
+        //         description: 'this is description',
+        //         type: 'MR',
+        //         avatar: 'https://avatars.githubusercontent.com/u/19267892?v=4',
+        //         url: 'https://gitlab.com',
+        //     },
+        //     {
+        //         id: 2,
+        //         title: 'this is title2',
+        //         description: 'this is description2',
+        //         type: 'MR',
+        //         avatar: 'https://avatars.githubusercontent.com/u/19267892?v=4',
+        //         url: 'https://gitlab.com',
+        //     },
+        // ];
+        
+        return `
+            <!DOCTYPE html>
+            <html lang="ja">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>WebView Example</title>
+                <link rel="stylesheet" href="${styleUri}">
+            </head>
+            <body>
+                <div class="card">
+                    <div class="card-header">
+                        <div class="flex-between mb-4">
+                            <div class="notification__repository">
+                                this is repository name
+                            </div>
+                            <div class="notification__badge">
+                                4
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="flex-start mb-4">
+                            <div class="notification__icon mr-4">
+                                <img src="https://avatars.githubusercontent.com/u/19267892?v=4" alt="Avatar" />
+                            </div>
+                            <div class="notification__title">
+                                this is title
+                            </div>
+                        </div>
+                        <div class="flex-start">
+                            <div class="notification__tag mr-4">
+                                MR
+                            </div>
+                            <div class="notification__type">
+                                you are assigned
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div id="output"></div>
+                <script src="${scriptUri}" />
+            </body>
+            </html>
         `;
     }
 }
-
