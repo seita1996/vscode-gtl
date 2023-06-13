fc69c08e32848ebc0434d367f3cd28da3f0ce175
seita1996 <taha.seita@gmail.com>
2023-06-13 23:16:21 +0900

    start polling when VSCode startup

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
 import * as vscode from 'vscode';
 import { TaskViewProvider } from './webview/task_view_provider';
+import { fetchTodos } from './gitlab_api/fetch_todos';
+import { showStatusBarNotificationBadge } from './statusbar/notification_badge';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -10,12 +12,52 @@ export function activate(context: vscode.ExtensionContext) {
   });
   context.subscriptions.push(activateSidebarDisposable);
 
+  const taskView = new TaskViewProvider(context.extensionUri);
 	context.subscriptions.push(
 		vscode.window.registerWebviewViewProvider(
 			'vscode-gitlab-task-list.notificationsView',
-			new TaskViewProvider(context.extensionUri)
+			taskView
 		)
 	);
+
+  _startPolling(taskView);
+}
+
+async function _startPolling(taskView: TaskViewProvider) {
+  const todos = await fetchTodos();
+  showStatusBarNotificationBadge(todos.length);
+  let beforeTodos = todos;
+
+  // Load the content of the view (1st load)
+  taskView.updateView(todos);
+
+  // Load the content of the view (every 10s)
+  setInterval(async () => {
+    const todos = await fetchTodos();
+    showStatusBarNotificationBadge(todos.length);
+    const incrementedTodos = _incrementedTodos(beforeTodos, todos);
+    incrementedTodos.forEach((todo: any) => {
+      const goToGitLab = "Go to GitLab";
+      vscode.window.showInformationMessage(`You are ${todo.action} at ${todo.project.name} ${todo.targetType}.`, goToGitLab)
+        .then((selection) => {
+          if (selection === goToGitLab) {
+            vscode.env.openExternal(vscode.Uri.parse(todo.target.webUrl));
+          }
+        }
+        );
+    });
+    taskView.updateView(todos);
+    beforeTodos = todos;
+  }, 10000);
+}
+
+function _incrementedTodos(beforeTodos: any, afterTodos: any) {
+  const diff = afterTodos.filter((afterTodo: any) => {
+    return !beforeTodos.some((beforeTodo: any) => {
+      return beforeTodo.id === afterTodo.id;
+    });
+  });
+  return diff;
 }
 
 export function deactivate() {}
--- src/webview/task_view_provider.ts
+++ src/webview/task_view_provider.ts
@@ -1,11 +1,10 @@
 import * as vscode from 'vscode';
-import { fetchTodos } from '../gitlab_api/fetch_todos';
 import { initGraphqlApi } from '../gitlab_api/init_graphql_api';
 import { ViewTodos } from './view_todos';
 import { settings } from '../settings';
-import { showStatusBarNotificationBadge } from '../statusbar/notification_badge';
 
 export class TaskViewProvider implements vscode.WebviewViewProvider {
+  wvv!: vscode.WebviewView;
   constructor(private readonly extensionUri: vscode.Uri) { }
 
   public resolveWebviewView(webviewView: vscode.WebviewView) {
@@ -15,65 +14,33 @@ export class TaskViewProvider implements vscode.WebviewViewProvider {
       enableCommandUris: true
     };
 
-    this._updateView(webviewView);
+    this.wvv = webviewView;
+
+    // Sets up an event listener to listen for messages passed from the webview view context
+    // and executes code based on the message that is recieved
+    this._setWebviewMessageListener();
   }
 
-  private async _updateView(webviewView: vscode.WebviewView) {
+  public async updateView(todos: any) {
     // Uri that enable to load ./public/index.css in the WebView
-    const styleUri = webviewView.webview.asWebviewUri(
+    const styleUri = this.wvv.webview.asWebviewUri(
       vscode.Uri.joinPath(this.extensionUri, "public", "index.css")
     ).toString();
-    const scriptUri = webviewView.webview.asWebviewUri(
+    const scriptUri = this.wvv.webview.asWebviewUri(
       vscode.Uri.joinPath(this.extensionUri, "public", "main.js")
     ).toString();
-    const codiconsCheckUri = webviewView.webview.asWebviewUri(
+    const codiconsCheckUri = this.wvv.webview.asWebviewUri(
       vscode.Uri.joinPath(this.extensionUri, "public", "codicons-check.svg")
     ).toString();
 
     const viewTodos = new ViewTodos(settings.host, styleUri, scriptUri, codiconsCheckUri);
 
-    const todos = await fetchTodos();
-    showStatusBarNotificationBadge(todos.length);
-    let beforeTodos = todos;
-
-    // Load the content of the view (1st load)
-    webviewView.webview.html = viewTodos.generate(todos);
-
-    // Sets up an event listener to listen for messages passed from the webview view context
-    // and executes code based on the message that is recieved
-    this._setWebviewMessageListener(webviewView);
-
-    // Load the content of the view (every 10s)
-    setInterval(async () => {
-      const todos = await fetchTodos();
-      showStatusBarNotificationBadge(todos.length);
-      const incrementedTodos = this._incrementedTodos(beforeTodos, todos);
-      incrementedTodos.forEach((todo: any) => {
-        const goToGitLab = "Go to GitLab";
-        vscode.window.showInformationMessage(`You are ${todo.action} at ${todo.project.name} ${todo.targetType}.`, goToGitLab)
-          .then((selection) => {
-            if (selection === goToGitLab) {
-              vscode.env.openExternal(vscode.Uri.parse(todo.target.webUrl));
-            }
-          }
-          );
-      });
-      webviewView.webview.html = viewTodos.generate(todos);
-      beforeTodos = todos;
-    }, 10000);
+    // Display the todos in the webview view
+    this.wvv.webview.html = viewTodos.generate(todos);
   }
 
-  private _incrementedTodos(beforeTodos: any, afterTodos: any) {
-    const diff = afterTodos.filter((afterTodo: any) => {
-      return !beforeTodos.some((beforeTodo: any) => {
-        return beforeTodo.id === afterTodo.id;
-      });
-    });
-    return diff;
-  }
-
-  private _setWebviewMessageListener(webviewView: vscode.WebviewView) {
-    webviewView.webview.onDidReceiveMessage((message) => {
+  private _setWebviewMessageListener() {
+    this.wvv.webview.onDidReceiveMessage((message) => {
       const command = message.command;
       const gid = message.gid;
 
